5af130cf1b022ae39b8726e236a5ed34edc54e00
JINFAN WANG <xtremeaud@hotmail.com>
2016-04-05 10:42:06 +0800

    Res

--- ExperimentCode/ResolutionModule.cs
+++ ExperimentCode/ResolutionModule.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 namespace ExperimentCode
 {
+    class NamespaceEntity
+    {
+        string SrcName;
+        string DstName;
+        string Values;
+        string Actions;
+    }
+
+    class Namespace
+    {
+        string InputFilter;
+        List<NamespaceEntity> EntityList = new List<NamespaceEntity>();
+        string DefaultActions;
+    }
     class ResolutionModule
     {
     }
