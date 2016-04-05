9769ea31fbdb22e625d5d95f24b0c7f6effd233e
XtremeAud <xtremeaud@gmail.com>
2016-04-05 13:24:14 +0800

    u

--- ExperimentCode/ResolutionModule.cs
+++ ExperimentCode/ResolutionModule.cs
@@ -6,20 +6,28 @@ using System.Threading.Tasks;
 
 namespace ExperimentCode
 {
-    class NamespaceEntity
+    public class NamespaceEntity
     {
-        string SrcName;
-        string DstName;
-        string Values;
-        string Actions;
+        public string SrcName;
+        public string DstName;
+        public string Values;
+        public string Actions;
     }
 
     public class Namespace
     {
-        string NSName;
-        string InputFilter;
-        List<NamespaceEntity> EntityList = new List<NamespaceEntity>();
-        string DefaultActions;
+        public string NSName;
+        public string InputFilter;
+        public List<NamespaceEntity> EntityList = new List<NamespaceEntity>();
+        public string DefaultActions;
+
+        public Namespace(string NSName, string InputFilter, string DefaultActions)
+        {
+            this.NSName = NSName;
+            this.InputFilter = InputFilter;
+            this.DefaultActions = DefaultActions;
+        }
+
     }
 
     public static class Namespaces
@@ -29,9 +37,25 @@ namespace ExperimentCode
 
     class ResolutionModule
     {
-        public static void CreatNS()
+        public static void CreatNS(string NSName, string InputFilter, string DefaultAcitons)
         {
-            ;
+            if (FindNS(NSName) != -1)
+            {
+                Namespaces.NamespaceList.Add(new Namespace(NSName, InputFilter, DefaultAcitons));
+            }
+        }
+
+        public static int FindNS(string NSName)
+        {
+            int Index = -1;
+            for (int index = 0; index < Namespaces.NamespaceList.Count; index++)
+            {
+                if (Namespaces.NamespaceList[index].NSName == NSName)
+                {
+                    return index;
+                }
+            }
+            return Index;
         }
     }
 }
